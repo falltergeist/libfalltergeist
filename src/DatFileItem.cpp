d0a543cd1fe4947e220afa4db1fddb243596e63c
alexeevdv <mail@alexeevdv.ru>
2013-01-11 15:04:55 +0400

    lil changes

--- src/DatFileItem.cpp
+++ src/DatFileItem.cpp
@@ -1,10 +1,46 @@
 #include "DatFileItem.h"
 
+#include "string.h"
+
 namespace Libfalltergeist
 {
 
 DatFileItem::DatFileItem()
 {
+    data = 0;
+    _filename = 0;
+    _dataOffset = 0;
+    _unpackedSize = 0;
+    _packedSize = 0;
+    _packed = false;
 }
+
+DatFileItem::~DatFileItem()
+{
+    delete [] data;
+    delete [] _filename;
+}
+
+/**
+ * Sets filename with path
+ * @brief DatFileItem::setFilename
+ * @param filename
+ */
+void DatFileItem::setFilename(char * filename)
+{
+    delete [] _filename;
+    _filename = new char[strlen(filename) + 1]();
+    strcpy(_filename,filename);
+}
+
+/**
+ * Returns filename with path
+ * @brief DatFileItem::getFilename
+ */
+char * DatFileItem::getFilename()
+{
+    return _filename;
+}
+
 
 }
--- src/DatFileItem.h
+++ src/DatFileItem.h
@@ -1,12 +1,38 @@
 #ifndef LIBFALLTERGEIST_DATFILEITEM_H
 #define LIBFALLTERGEIST_DATFILEITEM_H
+
 namespace Libfalltergeist
 {
 
 class DatFileItem
 {
+protected:
+    char * _filename; // filename with path (path/to/file.ext)
+    unsigned int _dataOffset; // data offset in the DAT file
+    unsigned int _unpackedSize; // unpacked data size
+    unsigned int _packedSize; // packed data size
+    bool _packed; // is packed?
 public:
+
+    char * data; // file data
+
     DatFileItem();
+    ~DatFileItem();
+
+    void setFilename(char * filename);
+    char * getFilename();
+
+    void setDataOffset(unsigned int offset);
+    unsigned int getDataOffset();
+
+    void setUnpackedSize(unsigned int size);
+    unsigned int getUnpackedSize();
+
+    void setPackedSize(unsigned int size);
+    unsigned int getPackedSize();
+
+    void setPacked(bool packed);
+    bool getPacked();
 };
 
 }
