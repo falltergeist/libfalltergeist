428841828e5450a50fcdfb02a0958fe3bf729e5e
alexeevdv <mail@alexeevdv.ru>
2014-11-11 15:20:51 +0300

    Code formatting

--- src/MsgFileType.h
+++ src/MsgFileType.h
@@ -1,6 +1,21 @@
-// DISCLAIMER.
-// This code was taken from the ACMReader plugin of the GemRB project (http://gemrb.org)
-// and then adapted for libfalltergeist. All credit goes to the original authors.
+/*
+ * Copyright 2012-2014 Falltergeist Developers.
+ *
+ * This file is part of Falltergeist.
+ *
+ * Falltergeist is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Falltergeist is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Falltergeist.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef LIBFALLTERGEIST_MSGFILETYPE_H
 #define LIBFALLTERGEIST_MSGFILETYPE_H
